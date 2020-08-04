b4685db995b6667bf8a219dc02327eedbeea97f7
Anna Brander <aelise17264@gmail.com>
2020-08-04 14:09:55 -0600

    added 4 event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,37 @@
 // Your code goes here
+let topImage = document.querySelector('img')
+function zoom(event){
+    event.preventDefault()
+
+    scale += event.deltaY * -0.01;
+
+    scale = Math.min(Math.max(.125, scale), 2);
+
+    topImage.style.transform = `scale(${scale})`;
+}
+
+let scale = 1;
+
+topImage.addEventListener('wheel', zoom);
+
+let title = document.querySelector('h1')
+title.addEventListener('mouseover', (event) => {
+    event.target.style.color = 'green';
+        
+    setTimeout(() => {
+        event.target.style.color = 'orange'
+    }, 1000)
+})
+
+let wholePage = document.querySelector('html')
+wholePage.addEventListener('keydown', event => {
+    if(event.key === 'Tab'){
+        event.target.style.background = '#86c4ba'
+    }else {
+        console.log('hit Tab...give it a try')
+    }
+})
+
+window.onload = (event) => {
+    console.log('You are currently viewing the whole page')
+}
